658803bc09d4fed0e610464327128331ba721fb4
JINDBS <jean.duquenne.ch@gmail.com>
2022-03-10 12:30:39 +0100

    added check to see if category is an empty string and not write it if it is the case

--- functions/src/createPostTrigger.js
+++ functions/src/createPostTrigger.js
@@ -1,34 +1,49 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 async function fetchCommunity(cid) {
   const communityRef = admin.firestore().collection("communities").doc(cid);
   const communityDocSnap = await communityRef.get();
-  return {communityRef, communityDocSnap};
+  return { communityRef, communityDocSnap };
 }
 
 /// Triggered by post collection create, this function initializes a new community
 /// in the community collection when the first post for a token is created.
 exports.createPost = functions.firestore
-  .document('posts/{docId}')
+  .document("posts/{docId}")
   .onCreate(async (snap, context) => {
     const post = snap.data();
 
-    const {communityRef, communityDocSnap} = await fetchCommunity(post.accessTokenId);
+    const { communityRef, communityDocSnap } = await fetchCommunity(
+      post.accessTokenId
+    );
 
     if (!communityDocSnap.exists) {
-      await communityRef.set({
-        categories: [{ name: post.category, count: 1 }],
-      });
-    } else {
-      let category = communityDocSnap.data().categories.find(item => item.name === post.category);
+      if (post.category !== "") {
+        await communityRef.set({
+          categories: [{ name: post.category, count: 1 }],
+        });
+      } else {
+        await communityRef.set({
+          categories: [],
+        });
+      }
+    } else if (post.category !== "") {
+      let category = communityDocSnap
+        .data()
+        .categories.find((item) => item.name === post.category);
       if (category === undefined) {
         category = { name: post.category, count: 1 };
       } else {
         category = { name: category.name, count: category.count + 1 };
       }
-      const updatedCategories = communityDocSnap.data().categories.filter(item => item.name !== post.category);
+      const updatedCategories = communityDocSnap
+        .data()
+        .categories.filter((item) => item.name !== post.category);
       updatedCategories.push(category);
-      await communityRef.set({ categories: updatedCategories }, { merge: true });
+      await communityRef.set(
+        { categories: updatedCategories },
+        { merge: true }
+      );
     }
-});
+  });
